c1e9481457f10cf48cdf60e493d4c2477d0e2e0d
Yuuki <6851027-yukiz@users.noreply.gitlab.com>
2023-09-28 17:39:43 +0800

    patch quest 3.2 v3

--- Tool/MergeQuests.js
+++ Tool/MergeQuests.js
@@ -26,7 +26,9 @@ if (!fs.existsSync(questPatchesDir)) {
 const unknownCondition = {
   type: "QUEST_COND_UNKNOWN",
   param: [0, 0],
+  param_str: ""
 };
+
 const questBlacklist = [
   "acceptCond",
   "finishCond",
@@ -97,6 +99,21 @@ for (const questPatch of questPatches) {
   }
 }
 
+// Function to remove duplicates
+function removeDuplicates(array) {
+  const uniqueArray = [];
+  array.forEach((item) => {
+    if (
+      !uniqueArray.some((existingItem) =>
+        JSON.stringify(existingItem) === JSON.stringify(item)
+      )
+    ) {
+      uniqueArray.push(item);
+    }
+  });
+  return uniqueArray;
+}
+
 /**
  * Returns a cleaned version of a condition/execution.
  *
@@ -106,20 +123,45 @@ function clean(condition) {
   let { type, param, param_str, count } = {
     type: condition._type ?? condition.type,
     param: condition._param ?? condition.param,
-    param_str: condition._param_str ?? condition.param_str,
+    param_str: condition._param_str ?? condition.param_str ?? condition.paramString,
     count: condition._count ?? condition["count"],
   };
 
   const object = {
     type,
     param: !param
-      ? []
+      ? [0, 0] // fix Index 0 out of bounds for length 0 (1=main quest id, 2=status?)
       : param.filter((param) => param !== null && param !== ""),
-    param_str: param_str ?? "",
+    param_str: param_str ?? "", // java.lang.NumberFormatException: For input string: "" (need fix in gc)
   };
 
   // Check for a 'count' parameter.
   if (count) object.count = count;
+
+  // make sure there is always status?
+  if (object.param.length >= 0) {
+    if (typeof object.param[0] == "number") {
+      if (object.param[1] == null) {
+        object.param[1] = 0;
+      }
+    } else if (object.param[0] == null) {
+      //console.log(object);
+      object.param[0] = 0;
+      object.param[1] = 0;
+    } else {
+      if (object.param[1] == null) {
+        object.param[1] = `0`;
+      }
+    }
+  } else {
+    object.param = [0, 0]
+  }
+
+  // skip
+  if(object.type == null){
+    object.param = [];
+    object.param_str = ""
+  }
 
   return object;
 }
@@ -189,9 +231,23 @@ const talks_data = JSON.parse(talks);
 const quests_config = [];
 const newQuests = [];
 const newQuestsNoFound = [];
+
 var count_patch32 = 0;
+var count_no_acceptCond = 0;
+var count_no_finishCond = 0;
+
+// quest data (in bin, new)
 for (const mainQuestData of mainQuest_data) {
   const mainQuestId = mainQuestData.id;
+
+  // dev tes
+  /*
+  if (mainQuestId != 3000) { // or 3000 or 347
+    // skip
+    continue;
+  }
+  */
+
   const binfile = `${binOutput}/${mainQuestId}.json`;
   const binfile32 = `${questPatches32Bin}/${mainQuestId}.json`;
 
@@ -199,60 +255,62 @@ for (const mainQuestData of mainQuest_data) {
 
   // Find all sub-quests for the main quest.
   let isNewQuest = false;
-  let subQuests = rel3_2_data.filter((i) => i.mainId === mainQuestId);
-  if (subQuests.length === 0) {
-    // This will be considered a new quest if no quest configuration is found in version 3.2. based on mainQuestData file data
+
+  // quest data (in config quest 3.2 old)
+  let main_data = rel3_2_data.filter((i) => i.mainId === mainQuestId);
+  if (main_data.length == 0) {
+
+    // This will be considered a new quest if no quest configuration is found in version 3.2. based on main_data file data
     isNewQuest = true;
 
-    // since not all new quests are in `QuestExcelConfigData` we have to look again in `Quest bin folder` so both should be there. and sometimes the `Quest Bin Folder` doesn't have new quest data so we have to look in `quest main` or `quest config` in `Excel folder`
-    subQuests = latest_data.filter((i) => i.mainId === mainQuestId);
-    if (subQuests.length === 0) {
-      console.log(`Looking for alternatives quest sub ${binfile}`);
+    // since not all new quests are in `QuestExcelConfigData` we have to look again in `Quest bin folder` so both should be there. and sometimes the `Quest Bin Folder` doesn't have new quest data so we have to look in `quest main` or `quest config` in `Excel folder` or use gio data?
+    var check_new = latest_data.filter((i) => i.mainId === mainQuestId); // Note: this as subQuests
+    if (check_new.length == 0) {
+
+      // if new not found, find alt
+
+      //process.exit()
       if (fs.existsSync(binfile)) {
-        const binsub_r = fs.readFileSync(binfile);
-        const binsub_d = JSON.parse(binsub_r);
-        let subQuestBin = binsub_d.subQuests;
-        if (subQuestBin !== undefined) {
-          subQuests = subQuestBin; // copy `subquest bin` to `subquest config`
+        const binsub_d = JSON.parse(fs.readFileSync(binfile));
+        let r = binsub_d.subQuests;
+        if (r !== undefined) {
+          //main_data.subQuests = r; // copy `bin sub quest` to `config main_data for sub quest` (copy as laset quest bin)
+        } else {
+          console.log(`not found sub quest alternatives quest main ${binfile}`);
         }
+      } else {
+        console.log(`not found alternatives quest main ${binfile}`);
       }
+
+    } else {
+      main_data = check_new // copy as 3.2
     }
-    if (subQuests.length !== 0) {
+
+    // count (subQuests)
+    if (main_data.length != 0) {
       newQuests.push(mainQuestId);
     } else {
       newQuestsNoFound.push(mainQuestId);
     }
+
   }
 
-  // find patch 3.2
-  var bin32;
-  if (fs.existsSync(binfile32)) {
-    bin32 = JSON.parse(fs.readFileSync(binfile32));
-  }
-
-  // Find all talks for the main quest.
+  // Find all talks for main quest.
   const talks = talks_data.filter((talk) => talk.questId === mainQuestId);
 
-  // dev testing
-  /*
-  if (mainQuestId != 3000) {
-    // skip
-    continue;
-  }
-  */
-
-  console.log("=====================================");
-  console.log(`Performing merge on main quest ${mainQuestId}.`);
-  console.log(`This quest is ${isNewQuest ? "new" : "old"}.`);
-  console.log(`There are ${subQuests.length} sub-quests.`);
-  console.log(`There are ${talks.length} talks.`);
-  console.log("=====================================");
-
-  // Check if the quest has sub-quests.
-  if (subQuests.length === 0) {
+  // Check if quest has sub-quests. (main_data as subQuests)
+  if (main_data.length == 0) {
     console.log(`Main quest ${mainQuestId} has no sub-quests, skipping...`);
     continue;
   }
+
+  //console.log("=====================================");
+  //console.log(`Performing merge on main quest ${mainQuestId}.`);
+  //console.log(`This quest is ${isNewQuest ? "new" : "old"}.`);
+  //console.log(`There are ${main_data.length} sub-quests.`);
+  //console.log(`There are ${talks.length} talks.`);
+  //console.log("=====================================");
+
   // Create the base quest data.
   const quest_bin = {
     /** @type number */ id: mainQuestId,
@@ -262,16 +320,47 @@ for (const mainQuestData of mainQuest_data) {
     /** @type number */ descTextMapHash: mainQuestData.descTextMapHash,
     /** @type string */ luaPath: mainQuestData.luaPath,
     /** @type string */ showType: mainQuestData.showType,
-    /** @type number[] */ suggestTrackMainQuestList:
-      mainQuestData.suggestTrackMainQuestList,
+    /** @type number[] */ suggestTrackMainQuestList: mainQuestData.suggestTrackMainQuestList,
     /** @type number[] */ rewardIdList: mainQuestData.rewardIdList,
     /** @type number[] */ chapterId: mainQuestData.chapterId,
     /** @type any[] */ subQuests: [],
     /** @type any[] */ talks: [],
   };
 
-  // Create sub-quests for the main quest.
-  for (const subQuestBin of subQuests) {
+  // find patch 3.2
+  var bin32;
+  if (fs.existsSync(binfile32)) {
+    bin32 = JSON.parse(fs.readFileSync(binfile32));
+  }
+
+  // Create sub-quests for quest.
+  for (const subQuestBin of main_data) {
+
+    // Patch all to bin quest, if found config 3.2 gio
+    const bin32config = bin32.subQuests.filter((item) => item.subId === subQuestBin.subId)[0];
+    if (bin32config) {
+
+      if (bin32config.acceptCond != null)
+        subQuestBin.acceptCond = bin32config.acceptCond;
+      if (bin32config.finishCond != null)
+        subQuestBin.finishCond = bin32config.finishCond;
+      if (bin32config.guide != null)
+        subQuestBin.guide = bin32config.guide;
+      if (bin32config.failCond != null)
+        subQuestBin.failCond = bin32config.failCond;
+      if (bin32config.beginExec != null)
+        subQuestBin.beginExec = bin32config.beginExec;
+      if (bin32config.finishExec != null)
+        subQuestBin.finishExec = bin32config.finishExec;
+      if (bin32config.failExec != null)
+        subQuestBin.failExec = bin32config.failExec;
+      if (bin32config.acceptCondComb != null)
+        subQuestBin.acceptCondComb = bin32config.acceptCondComb;
+      if (bin32config.finishCondComb != null)
+        subQuestBin.finishCondComb = bin32config.finishCondComb;
+
+      count_patch32++;
+    }
 
     // sub config
     const subQuest_config = {
@@ -287,19 +376,13 @@ for (const mainQuestData of mainQuest_data) {
     } = subQuestBin;
 
     if (acceptCond) {
-      subQuest_config.acceptCond = acceptCond
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.acceptCond = removeDuplicates(acceptCond.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
     if (finishCond) {
-      subQuest_config.finishCond = finishCond
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.finishCond = removeDuplicates(finishCond.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
     if (failCond) {
-      subQuest_config.failCond = failCond
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.failCond = removeDuplicates(failCond.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
 
     // Validate executions.
@@ -310,100 +393,50 @@ for (const mainQuestData of mainQuest_data) {
     } = subQuestBin;
 
     if (beginExec) {
-      subQuest_config.beginExec = beginExec
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.beginExec = removeDuplicates(beginExec.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
     if (finishExec) {
-      subQuest_config.finishExec = finishExec
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.finishExec = removeDuplicates(finishExec.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
     if (failExec) {
-      subQuest_config.failExec = failExec
-        .filter((cond) => cond._type !== null || cond.type !== null)
-        .map(clean);
+      subQuest_config.failExec = removeDuplicates(failExec.filter((cond) => cond._type !== null || cond.type !== null).map(clean));
     }
-
-    // Check if the quest is new.
-    if (isNewQuest) {
-      subQuestBin.acceptCond = [unknownCondition];
-      subQuest_config.acceptCond = [unknownCondition];
-    }
-
-    // patch 3.2
-    if (subQuestBin.acceptCond[0].type == 'QUEST_COND_UNKNOWN' || subQuest_config.acceptCond[0].type == 'QUEST_COND_UNKNOWN') {
-      const bin32config = bin32.subQuests.filter((item) => item.subId === subQuestBin.subId)[0];
-      if (bin32config) {
-        subQuestBin.acceptCond = bin32config.acceptCond;
-        subQuestBin.finishCond = bin32config.finishCond;
-        subQuestBin.guide = bin32config.guide;
-
-        subQuest_config.acceptCond = bin32config.acceptCond;
-        subQuest_config.finishCond = bin32config.finishCond;
-        subQuest_config.guide = bin32config.guide;
-
-        count_patch32++;
-
-        //console.log(bin32config)
-        //process.exit();
-      }
-    }
-
-   // console.log(subQuest_config)
 
     // fix (Expected a string but was BEGIN_OBJECT)
-    if (typeof subQuestBin.acceptCondComb === "object") {
-      subQuestBin.acceptCondComb = "LOGIC_NONE"; // ???
-    }
     if (typeof subQuest_config.acceptCondComb === "object") {
       subQuest_config.acceptCondComb = "LOGIC_NONE"; // ???
     }
-
-    if (typeof subQuestBin.finishCondComb === "object") {
-      subQuestBin.finishCondComb = "LOGIC_NONE"; // ???
-    }
     if (typeof subQuest_config.finishCondComb === "object") {
       subQuest_config.finishCondComb = "LOGIC_NONE"; // ???
-    }
-
-    if (typeof subQuestBin.failCondComb === "object") {
-      subQuestBin.failCondComb = "LOGIC_NONE"; // ???
     }
     if (typeof subQuest_config.failCondComb === "object") {
       subQuest_config.failCondComb = "LOGIC_NONE"; // ???
     }
 
-    
-
     // fix null
-    if (subQuestBin.finishCond == null) {
-      subQuestBin.finishCond = []; // ???
+    // acceptCond (this is always there)
+    if (subQuest_config.acceptCond == null) {
+      subQuest_config.acceptCond = [unknownCondition];
+      console.log(`no acceptCond for ${mainQuestId} / ${subQuestBin.subId}`);
+      count_no_acceptCond++;
     }
+    // finishCond (this is always there)
     if (subQuest_config.finishCond == null) {
-      subQuest_config.finishCond = []; // ???
+      subQuest_config.finishCond = [unknownCondition];
+      console.log(`no finishCond for ${mainQuestId} / ${subQuestBin.subId}`);
+      count_no_finishCond++;
     }
-    if (subQuestBin.failCond == null) {
-      subQuestBin.failCond = []; // ???
-    }
+
+    // - ot -
+
     if (subQuest_config.failCond == null) {
       subQuest_config.failCond = []; // ???
-    }
-
-    if (subQuestBin.beginExec == null) {
-      subQuestBin.beginExec = []; // ???
     }
     if (subQuest_config.beginExec == null) {
       subQuest_config.beginExec = []; // ???
     }
-    if (subQuestBin.finishExec == null) {
-      subQuestBin.finishExec = []; // ???
-    }
     if (subQuest_config.finishExec == null) {
       subQuest_config.finishExec = []; // ???
-    }
-    if (subQuestBin.failExec == null) {
-      subQuestBin.failExec = []; // ???
     }
     if (subQuest_config.failExec == null) {
       subQuest_config.failExec = []; // ???
@@ -421,12 +454,12 @@ for (const mainQuestData of mainQuest_data) {
     // Remove fields which are empty. (in config quest)
     removeFields(subQuest_config, questBlacklist);
 
-    // Check if quest has any patches. (in  config quest)
+    // Check if quest has any patches. (in config quest)
     if (patches_data[subQuestBin.subId]) {
       Object.assign(subQuest_config, patches_data[subQuestBin.subId]);
     }
 
-     //console.log(subQuest_config)
+    //console.log(subQuest_config)
 
     // Add to the main quest's collection.
     const subQuestForMain = Object.assign({}, subQuest_config);
@@ -453,15 +486,13 @@ for (const mainQuestData of mainQuest_data) {
     } = talkData;
 
     if (beginCond) {
-      talk.beginCond = beginCond.filter((cond) => cond.type != null).map(clean);
+      talk.beginCond = removeDuplicates(beginCond.filter((cond) => cond.type != null).map(clean));
     }
     if (finishCond) {
-      talk.finishCond = finishCond
-        .filter((cond) => cond.type != null)
-        .map(clean);
+      talk.finishCond = removeDuplicates(finishCond.filter((cond) => cond.type != null).map(clean));
     }
     if (failCond) {
-      talk.failCond = failCond.filter((cond) => cond.type != null).map(clean);
+      talk.failCond = removeDuplicates(failCond.filter((cond) => cond.type != null).map(clean));
     }
 
     // Validate executions.
@@ -472,15 +503,13 @@ for (const mainQuestData of mainQuest_data) {
     } = talkData;
 
     if (beginExec) {
-      talk.beginExec = beginExec.filter((cond) => cond.type != null).map(clean);
+      talk.beginExec = removeDuplicates(beginExec.filter((cond) => cond.type != null).map(clean));
     }
     if (finishExec) {
-      talk.finishExec = finishExec
-        .filter((cond) => cond.type != null)
-        .map(clean);
+      talk.finishExec = removeDuplicates(finishExec.filter((cond) => cond.type != null).map(clean));
     }
     if (failExec) {
-      talk.failExec = failExec.filter((cond) => cond.type != null).map(clean);
+      talk.failExec = removeDuplicates(failExec.filter((cond) => cond.type != null).map(clean));
     }
 
     // Remove un-used fields.
@@ -511,7 +540,9 @@ console.log("=====================================");
 console.log(`There are ${quests_config.length} quests.`);
 console.log(`There are ${newQuests.length} new quests.`);
 console.log(`There are ${newQuestsNoFound.length} quests not found.`);
-console.log(`There are ${count_patch32} data sub quest without acceptance conditions`);
+console.log(`There are ${count_patch32} sub quest use bin 3.2 gio`);
+console.log(`There are ${count_no_acceptCond} sub quest with no acceptCond`);
+console.log(`There are ${count_no_finishCond} sub quest with no finishCond`);
 
 for (let i = 0; i < newQuests.length; i += 9) {
   const newQuestsSlice = newQuests.slice(i, i + 9);
